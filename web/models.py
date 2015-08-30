7dcbea4e2165a812b3a722fc3488c5b0f8eba743
Artemis <aleeodm25@gmail.com>
2015-08-30 00:10:16 -0400

    add volume to model

--- web/models.py
+++ web/models.py
@@ -13,8 +13,9 @@ class Box(db.Model):
     x = db.Column(db.Integer, nullable=False)
     y = db.Column(db.Integer, nullable=False)
     z = db.Column(db.Integer, nullable=False)
-
+    volume = db.Column(db.Integer, nullable=False)
     def __init__(self, x=1,y=1,z=1):
         self.x = x
         self.y = y
         self.z = z
+        self.volume = x*y*z
