3b522dbb5774eb3510b66b24900b64060635e65c
Artemis <aleeodm25@gmail.com>
2015-10-21 18:22:04 -0400

    basic modal working code still ugly

--- web/static/app/app.module.js
+++ web/static/app/app.module.js
@@ -1,13 +1,13 @@
-angular.module('Todo',[])
+angular.module('boxing',['ui.bootstrap'])
     .config(['$interpolateProvider', function($interpolateProvider) {
         $interpolateProvider.startSymbol('{['); //required to work with jinja2
         $interpolateProvider.endSymbol(']}');
     }])
     .controller('boxCtrl', ['$scope', '$http', function($scope, $http){
-        $scope.todos = [];
+        $scope.items = [];
 
         $scope.addItem = function(){
-            $scope.todos.push({
+            $scope.items.push({
                 'length':$scope.length,
                 'width':$scope.width,
                 'height':$scope.height,
@@ -19,17 +19,61 @@ angular.module('Todo',[])
             $scope.amount = '';
         };
         $scope.removeItem = function(index){
-            $scope.todos.splice(index,1);
+            $scope.items.splice(index,1);
         };
         $scope.boxOrder = function(){
 
-            $http.post('/api/box_order',$scope.todos)
+            $http.post('/api/box_order',$scope.items)
                 .success(function(response){
                     $scope.order_volume=response;
                 });
 
         };
         $scope.clearAll = function(){
-            $scope.todos = [];
+            $scope.items = [];
         }
-    }]);
+    }])
+    .controller('modalController', function($scope, $uibModal, $log) {
+
+        $scope.animationsEnabled = true;
+
+        $scope.open = function (size) {
+            var modalInstance = $uibModal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: '/static/app/modalContent.html',
+                controller: 'ModalInstanceController',
+                size: size,
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function () {  // close() was called
+                $scope.clearAll();
+            }, function () {  // close wasn't called
+                // do nothing
+            });
+        };
+
+        $scope.toggleAnimation = function () {
+            $scope.animationsEnabled = !$scope.animationsEnabled;
+        };
+    })
+    .controller('ModalInstanceController', function ($scope, $http, $modalInstance, items) {
+
+        $http.post('/api/box_order',items)
+            .success(function(response){
+                $scope.order_volume=response;
+            });
+
+
+        $scope.new_order = function () {
+            $modalInstance.close();
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    });
